e1c108fe5339b707e9b0370e740efce0c82f7075
Rushikesh_Salunke <officialtusharbhawar2003@gmail.com>
2024-06-23 18:48:35 +0530

    Day 4 push

--- Day03_TimeComplexity/SumOFNaturalNumbers_2.java
+++ Day03_TimeComplexity/SumOFNaturalNumbers_2.java
@@ -7,12 +7,12 @@ public class SumOFNaturalNumbers_2 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number : ");
         int num = sc.nextInt();
-        int result = SumOfNatralNumber(num);
+        int result = SumOfNaturalNumber(num);
         System.out.println("Total Sum Of Numbers -> "+result);
     }
     //this is Optimized Approach here we Used to O(1)
     //there in Constant time we Used
-    private static int SumOfNatralNumber(int num) {
+    private static int SumOfNaturalNumber(int num) {
           int itr=0;
           int sum=(num * (num+1))/2;
           System.out.println("Iteration -> "+itr);
